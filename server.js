de33e2ebc326608681e02fa8427448e4b04cdcdb
Greyson Mainor <gbmainor98@outlook.com>
2021-05-18 23:58:29 -0400

    set up my app.js to route to the pages on my app.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,7 +11,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 // Define API routes here
 app.use(routes);
 // Send every other request to the React app
