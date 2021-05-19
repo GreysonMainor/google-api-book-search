d22055305451ba33c53c7bf2e6c63e588ac7cc72
Greyson Mainor <gbmainor98@outlook.com>
2021-05-19 00:19:56 -0400

    Added Navbar on both pages.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../components/Grid/Grid";
+import Nav from "../components/Nav/Nav";
 
 class Home extends Component {
     render() {
         return (
             <div>
+                <Nav />
                 Welcome to the home page.
             </div>
         )
     }
 }
-
 export default Home
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { Col, Row, Container } from "../components/Grid/Grid";
+import Nav from "../components/Nav/Nav";
 
 class Saved extends Component {
     render() {
         return (
             <div>
+                <Nav />
                 Welcome to the saved page.
             </div>
         )
     }
 }
-
 export default Saved
