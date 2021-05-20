b671ba645c2d1f43c1e49ef58b1e0719d8a80dbb
Greyson Mainor <gbmainor98@outlook.com>
2021-05-20 13:26:27 -0400

    trying to get mongo to work.

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const path = require("path");
-const routes = require("./routes");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,8 +9,15 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Define API routes here
 app.use(routes);
+var MONGODB_URI = process.env.MONGODB_URI ||  "mongodb+srv://greymain:<password>@googlebooks.wmbed.mongodb.net/test";
+
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
