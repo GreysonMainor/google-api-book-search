8a43653cb7b55ae8a8d100422576cf55549f2f86
Greyson Mainor <gbmainor98@outlook.com>
2021-05-20 20:02:55 -0400

    updated readme.

---
+++ client/build/precache-manifest.4e6f7b1b72bde8bd3603350325f2924e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce3f816fd72f1745ce43",
+    "url": "/static/css/main.e5bddec1.chunk.css"
+  },
+  {
+    "revision": "ce3f816fd72f1745ce43",
+    "url": "/static/js/main.52be0094.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "34f8396342fafb41a7fc",
+    "url": "/static/js/2.ebd4028c.chunk.js"
+  },
+  {
+    "revision": "e5fe2b5806a50f10d7ff1707b1802ec6",
+    "url": "/index.html"
+  }
+];
